539e567c7afbbaa542a468e9e301c7e5c6080416
DmitriVanGuard <dimavanguard@gmail.com>
2018-12-10 18:08:24 +0200

    Server Port as env var

--- server/index.js
+++ server/index.js
@@ -3,6 +3,6 @@ var app = express()
 
 app.use(express.static('build'))
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000!')
+app.listen(process.env.PORT, () => {
+  console.log(`Listening on port ${process.env.PORT}!`)
 })
