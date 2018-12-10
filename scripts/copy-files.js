7d51da942d9d35e7da2edbbebb054a44381bcc9a
DmitriVanGuard <dimavanguard@gmail.com>
2018-12-10 13:08:48 +0200

    Updating package json version script

--- scripts/copy-files.js
+++ scripts/copy-files.js
@@ -9,6 +9,26 @@ const copyAtBuild = () => {
   ]
 
   filesToCopyFromSrcRoot.forEach(fileName => {
+    if (fileName === 'package.json') {
+      const packageData = fs.readFileSync(
+        path.resolve(__dirname, '..', 'src', 'package.json')
+      )
+
+      if (!packageData) throw 'Could not read file'
+
+      const jsonData = JSON.parse(packageData.toString())
+      const version = jsonData.version.split('.')
+      version[1] = parseInt(version[1], 10) + 1
+      jsonData.version = version.join('.')
+
+      fs.writeFileSync(
+        path.resolve(__dirname, '..', 'src', 'package.json'),
+        JSON.stringify(jsonData)
+      )
+
+      console.log('package.json file version updated')
+    }
+
     fs.copyFile(
       path.resolve(__dirname, '..', 'src', fileName),
       path.resolve(__dirname, '..', 'build', fileName),
