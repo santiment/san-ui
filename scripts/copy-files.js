4ad4e15db291142c89ea6b55a0cb0077555e323a
DmitriVanGuard <dimavanguard@gmail.com>
2018-12-10 15:02:42 +0200

    Junk clean. Incrementing published version

--- scripts/copy-files.js
+++ scripts/copy-files.js
@@ -2,28 +2,15 @@ const fs = require('fs')
 const path = require('path')
 const exec = require('child_process').execSync
 
-const copyAtBuild = () => {
-  const filesToCopyFromSrcRoot = [
-    'mixins.scss',
-    'variables.scss',
-    'package.json'
-  ]
+const filesToCopyFromSrcRoot = ['mixins.scss', 'variables.scss', 'package.json']
 
-  filesToCopyFromSrcRoot.forEach(fileName => {
-    if (fileName === 'package.json') {
-      const result = exec('cd src && yarn version --patch').toString()
-      console.log(result)
+filesToCopyFromSrcRoot.forEach(fileName => {
+  fs.copyFile(
+    path.resolve(__dirname, '..', 'src', fileName),
+    path.resolve(__dirname, '..', 'pckg-build', fileName),
+    err => {
+      if (err) throw err
+      console.log(`👍 ${fileName} was copied into build`)
     }
-
-    fs.copyFile(
-      path.resolve(__dirname, '..', 'src', fileName),
-      path.resolve(__dirname, '..', 'pckg-build', fileName),
-      err => {
-        if (err) throw err
-        console.log(`👍 ${fileName} was copied into build`)
-      }
-    )
-  })
-}
-
-copyAtBuild()
+  )
+})
---
+++ scripts/update-version.js
@@ -0,0 +1,12 @@
+const fs = require('fs')
+const path = require('path')
+const exec = require('child_process').execSync
+
+const onlineVersion = exec('npm show @santiment-network/ui version')
+  .toString()
+  .slice(0, -1)
+const result = exec(
+  `cd pckg-build && yarn version --new-version ${onlineVersion} --no-git-tag-version && yarn version --patch --no-git-tag-version`
+).toString()
+console.log(result)
+// console.log(result)
