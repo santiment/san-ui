220cf12588ce8123efa6e418100056d340b0a5f6
Yura Zatcepin <yura.zatsepin@gmail.com>
2018-12-21 22:33:11 +0300

    Add info addon

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,17 @@
-import { configure } from '@storybook/react';
+import React from 'react'
+import { configure, addDecorator } from '@storybook/react'
+import { withInfo, setDefaults } from '@storybook/addon-info'
 
-function loadStories() {
-  require('../stories');
+// addon-info
+setDefaults({
+  header: false
+})
+
+addDecorator(withInfo)
+addDecorator(story => <div style={{ padding: '1em' }}>{story()}</div>)
+
+function loadStories () {
+  require('../stories/index.js')
 }
 
-configure(loadStories, module);
+configure(loadStories, module)
