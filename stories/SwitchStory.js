46621dfa6454ebfcc70ee207433d605a033be3a5
DmitriVanGuard <dimavanguard@gmail.com>
2019-01-07 17:35:37 +0300

    Switch State Wrapper

--- stories/SwitchStory.js
+++ stories/SwitchStory.js
@@ -1,13 +1,32 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { storiesOf } from '@storybook/react'
 import Switch from '../src/Switch'
 import ColorModeComparison from './ColorModeComparison'
 
+class StateWrapper extends Component {
+  state = {
+    isSwitched: this.props.render.props.isSwitched
+  }
+
+  onClick = () => {
+    this.setState({ isSwitched: !this.state.isSwitched })
+  }
+
+  render () {
+    return React.cloneElement(this.props.render, {
+      onClick: this.onClick,
+      isSwitched: this.state.isSwitched
+    })
+  }
+}
+
 storiesOf('Switch', module).add('Simple', () => (
   <div>
     <ColorModeComparison>
-      <Switch option1='First' option2='Second' />
-      <Switch option1='First' option2='Second' isSwitched />
+      <StateWrapper render={<Switch option1='First' option2='Second' />} />
+      <StateWrapper
+        render={<Switch option1='First' option2='Second' isSwitched />}
+      />
     </ColorModeComparison>
   </div>
 ))
