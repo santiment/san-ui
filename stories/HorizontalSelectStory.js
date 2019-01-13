c8c6cb78fb1249f2fadcdf48823e0e49952afea4
Dmitri Marov <dimavanguard@gmail.com>
2019-01-13 23:00:09 +0300

    Update HorizontalSelectStory.js

--- stories/HorizontalSelectStory.js
+++ stories/HorizontalSelectStory.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
 import { HorizontalSelect } from '../src/Select'
-
 import ColorModeComparison from './ColorModeComparison'
 
 storiesOf('HorizontalSelect', module).add('Simple', () => (
