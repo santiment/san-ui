f62544a47825831be131fbc47c7f3b6e26a26287
DmitriVanGuard <dimavanguard@gmail.com>
2019-01-13 20:42:54 +0300

    Action logging story

--- stories/SwitchStory.js
+++ stories/SwitchStory.js
@@ -1,41 +1,23 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { storiesOf } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
 import Switch from '../src/Switch/Select'
+
 import ColorModeComparison from './ColorModeComparison'
 
-class StateWrapper extends Component {
-  state = {
-    isSwitched: this.props.render.props.isSwitched
-  }
-
-  onClick = () => {
-    this.setState({ isSwitched: !this.state.isSwitched })
-  }
-
-  render() {
-    return React.cloneElement(this.props.render, {
-      onClick: this.onClick,
-      isSwitched: this.state.isSwitched
-    })
-  }
-}
-
-/* storiesOf('Switch', module).add('Simple', () => (
- *   <div>
- *     <ColorModeComparison>
- *       <StateWrapper render={<Switch options={['First', 'Second']} />} />
- *       <StateWrapper
- *         render={<Switch options={['First', 'Second']} isSwitched />}
- *       />
- *     </ColorModeComparison>
- *   </div>
- * )) */
 storiesOf('Switch', module).add('Simple', () => (
   <div>
     <ColorModeComparison>
-      <Switch options={['First', 'Second']} />
-      <Switch options={['First', 'Second']} selectedIndex={1} />
-      <Switch options={['First', 'Second', 'Third']} />
+      <Switch options={['First', 'Second']} onSelect={action('Selected')} />
+      <Switch
+        options={['First', 'Second']}
+        selectedIndex={1}
+        onSelect={action('Selected')}
+      />
+      <Switch
+        options={['First', 'Second', 'Third']}
+        onSelect={action('Selected')}
+      />
     </ColorModeComparison>
   </div>
 ))
