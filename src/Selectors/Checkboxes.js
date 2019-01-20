a000ffb524f35b12655089da9103ef8133d68837
DmitriVanGuard <dimavanguard@gmail.com>
2019-01-20 17:48:04 +0300

    Checkbox checkmark styles

--- src/Selectors/Checkboxes.js
+++ src/Selectors/Checkboxes.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import LabeledSelector from './LabeledSelect'
 import { toggleMultiple } from './BaseSelect'
+import Icon from '../Icon'
 import styles from './Checkboxes.module.scss'
 
-const ContentContainer = () => <div className={styles.btn} />
+const ContentContainer = () => (
+  <div className={styles.btn}>
+    <Icon type='checkmark' className={styles.checkmark} />
+  </div>
+)
 
 const Checkboxes = ({ options, className, ...props }) => (
   <LabeledSelector
