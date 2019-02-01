81a3053fdf758b58073e8cb1faca4d5c12ed2527
DmitriVanGuard <dimavanguard@gmail.com>
2019-02-01 14:16:14 +0300

    New context architecture. Icon stories updated - now it shows what icon type is used

--- stories/IconStory.js
+++ stories/IconStory.js
@@ -3,25 +3,39 @@ import { storiesOf } from '@storybook/react'
 import Icon, { icons } from '../src/Icon'
 
 storiesOf('Icon', module).add('All', () => (
-  <div style={{ display: 'flex' }}>
+  <div style={{ display: 'flex', flexWrap: 'wrap' }}>
     {Object.keys(icons).map(iconKey => {
       return (
         <div
           key={iconKey}
           style={{
-            padding: '11px',
-            background: '#f7f8f9',
-            borderRadius: '4px',
-            width: 40,
-            height: 40,
-            margin: '0 10px 10px 0',
-            boxSizing: 'border-box',
             display: 'inline-flex',
-            justifyContent: 'center',
-            alignItems: 'center'
+            flexDirection: 'column',
+            alignItems: 'center',
+            padding: 4,
+            margin: '0 10px 10px 0',
+            minWidth: 60,
+            fontSize: 11,
+            flex: '0 0 6%'
           }}
         >
-          <Icon type={iconKey} fill='#000' />
+          <div
+            style={{
+              padding: '11px',
+              background: '#f7f8f9',
+              borderRadius: '4px',
+              width: 40,
+              height: 40,
+              marginBottm: 5,
+              boxSizing: 'border-box',
+              display: 'inline-flex',
+              justifyContent: 'center',
+              alignItems: 'center'
+            }}
+          >
+            <Icon type={iconKey} fill='#000' />
+          </div>
+          {iconKey}
         </div>
       )
     })}
