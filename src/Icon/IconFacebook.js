7aa96f326d02a3d9d064cb3a6c5d87f5e4a7910a
DmitriVanGuard <dimavanguard@gmail.com>
2019-02-27 16:18:35 +0300

    Removed default fill prop for some icons

--- src/Icon/IconFacebook.js
+++ src/Icon/IconFacebook.js
@@ -6,7 +6,6 @@ const IconFacebook = props => {
       width='10'
       height='16'
       viewBox='0 0 10 16'
-      fill='none'
       xmlns='http://www.w3.org/2000/svg'
       {...props}
     >
--- src/Icon/IconTwitter.js
+++ src/Icon/IconTwitter.js
@@ -6,7 +6,6 @@ const IconTwitter = props => {
       width='16'
       height='13'
       viewBox='0 0 16 13'
-      fill='none'
       xmlns='http://www.w3.org/2000/svg'
       {...props}
     >
