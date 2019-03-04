2593c6c110ff1c847a66960256a3a4a27dccb8b1
devanshu <devanshu.g@santiment.net>
2019-03-04 16:08:10 +0530

    Added stories for various variations of notifications

--- stories/NotificationStory.js
+++ stories/NotificationStory.js
@@ -7,11 +7,37 @@ const notifications = [
   {
     id: 1,
     description: 'This is a simple text description',
-    title: 'Big Bold Title',
+    title: 'Error',
+    variant: 'error'
   },
   {
     id: 2,
+    description: 'This is a simple text description',
+    title: 'Info',
+    variant: 'info'
+  },
+  {
+    id: 3,
+    description: 'This is a simple text description',
+    title: 'Warn',
+    variant: 'warn'
+  },
+  {
+    id: 4,
+    description: 'This is a simple text description',
+    title: 'Success',
+    variant: 'success'
+  },
+  {
+    id: 5,
+    description: 'This is a simple text description',
+    title: 'A really long notification title that will let the text go down to the next line',
+    variant: 'error'
+  },
+  {
+    id: 6,
     description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+    variant: 'warn'
   },
 ];
 
