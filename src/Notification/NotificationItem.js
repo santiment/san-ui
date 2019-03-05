217da982a72e28d9427b110009db52d574fd7c36
devanshu <devanshu.g@santiment.net>
2019-03-05 11:58:14 +0530

    Auto remove notification after timeout

--- src/Notification/NotificationItem.js
+++ src/Notification/NotificationItem.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import cx from 'classnames'
 import Icon from '../Icon';
@@ -12,52 +12,73 @@ const titleIcons = {
   success: 'checkmark-round',
 }
 
-const NotificationItem = ({
-  title,
-  titleIconName,
-  hideTitleIcon,
-  description,
-  variant,
-  onClose,
-}) => (
-  <Panel variant="tooltip" className={cx(styles.wrapper, styles[variant])}>
-    {!hideTitleIcon && (
-      <Icon
-        type={titleIconName || titleIcons[variant]}
-        className={cx(styles.icon, styles.titleIcon)}
-      />
-    )}
-    <div className={styles.messageArea}>
-      {title && (
-        <div className={styles.title}>
-          {title}
-        </div>
-      )}
-      {description && (
-        <div className={styles.description}>
-          {description}
-        </div>
-      )}
-    </div>
-    <Icon
-      type="close"
-      onClick={onClose}
-      className={cx(styles.icon, styles.closeIcon)}
-    />
-  </Panel>
-)
+class NotificationItem extends Component {
+  static propTypes = {
+    description: PropTypes.string,
+    hideTitleIcon: PropTypes.bool,
+    titleIconName: PropTypes.string,
+    onClose: PropTypes.func,
+    variant: PropTypes.oneOf(['info', 'warn', 'success', 'error']),
+    timeout: PropTypes.number,
+    title: PropTypes.string
+  }
 
-NotificationItem.propTypes = {
-  description: PropTypes.string,
-  hideTitleIcon: PropTypes.bool,
-  titleIconName: PropTypes.string,
-  onClose: PropTypes.func,
-  variant: PropTypes.oneOf(['info', 'warn', 'success', 'error']),
-  title: PropTypes.string
-}
+  defaultProps = {
+    hideTitleIcon: false,
+  }
 
-NotificationItem.defaultProps = {
-  hideTitleIcon: false,
+  constructor(props) {
+    super(props)
+
+    if (props.timeout) {
+      this.timeout = setTimeout(props.onClose, props.timeout)
+    }
+  }
+
+  componentWillUnmount() {
+    if (this.timeout) {
+      clearTimeout(this.timeout)
+    }
+  }
+
+  render() {
+    const {
+      title,
+      titleIconName,
+      hideTitleIcon,
+      description,
+      variant,
+      onClose,
+    } = this.props;
+
+    return (
+      <Panel variant="tooltip" className={cx(styles.wrapper, styles[variant])}>
+        {!hideTitleIcon && (
+          <Icon
+            type={titleIconName || titleIcons[variant]}
+            className={cx(styles.icon, styles.titleIcon)}
+          />
+        )}
+        <div className={styles.messageArea}>
+          {title && (
+            <div className={styles.title}>
+              {title}
+            </div>
+          )}
+          {description && (
+            <div className={styles.description}>
+              {description}
+            </div>
+          )}
+        </div>
+        <Icon
+          type="close"
+          onClick={onClose}
+          className={cx(styles.icon, styles.closeIcon)}
+        />
+      </Panel>
+    )
+  }
 }
 
 export default NotificationItem
