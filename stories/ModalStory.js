725cc8a225194f30f3a5b18774b47fec9c2fd0ee
devanshu <devanshu.g@santiment.net>
2019-03-11 16:59:54 +0530

    Added color mode comparison

--- stories/ModalStory.js
+++ stories/ModalStory.js
@@ -1,62 +1,67 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
+import ColorModeComparison from './ColorModeComparison'
 import Modal, { ModalContent, ModalActions } from "../src/Modal";
 import Button from "../src/Button";
 import styles from './ModalStory.module.scss';
 
 storiesOf("Modal", module)
   .add("default", () => (
-    <Modal
-      trigger={<Button>Show</Button>}
-      title="Lorem Ipsum"
-    >
-      <ModalContent>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, quaerat? A quos ab pariatur fugiat blanditiis, esse eum odit eligendi exercitationem voluptatem quod maiores nesciunt sapiente modi dolorem nisi accusamus architecto eius ipsa facere soluta? Magni nisi fuga voluptate, velit voluptas, eaque nobis cum deserunt eligendi reiciendis unde sit nesciunt.
-      </ModalContent>
-      <ModalActions>
-        <Button
-          border
-          className={styles.buttonCancel}
-          accent="negative"
-          onClick={() => alert("you pressed Cancel")}
-        >
-          Cancel
-        </Button>
-        <Button
-          variant="fill"
-          accent="positive"
-          onClick={() => alert("you pressed OK")}
-        >
-          OK
-        </Button>
-      </ModalActions>
-    </Modal>
+    <ColorModeComparison>
+      <Modal
+        trigger={<Button>Show</Button>}
+        title="Lorem Ipsum"
+      >
+        <ModalContent>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, quaerat? A quos ab pariatur fugiat blanditiis, esse eum odit eligendi exercitationem voluptatem quod maiores nesciunt sapiente modi dolorem nisi accusamus architecto eius ipsa facere soluta? Magni nisi fuga voluptate, velit voluptas, eaque nobis cum deserunt eligendi reiciendis unde sit nesciunt.
+        </ModalContent>
+        <ModalActions>
+          <Button
+            border
+            className={styles.buttonCancel}
+            accent="negative"
+            onClick={() => alert("you pressed Cancel")}
+          >
+            Cancel
+          </Button>
+          <Button
+            variant="fill"
+            accent="positive"
+            onClick={() => alert("you pressed OK")}
+          >
+            OK
+          </Button>
+        </ModalActions>
+      </Modal>
+    </ColorModeComparison>
   ))
   .add('without close icon', () => (
-    <Modal
-      trigger={<Button>Show</Button>}
-      title="Lorem Ipsum"
-      hideCloseIcon
-    >
-      <ModalContent>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, quaerat? A quos ab pariatur fugiat blanditiis, esse eum odit eligendi exercitationem voluptatem quod maiores nesciunt sapiente modi dolorem nisi accusamus architecto eius ipsa facere soluta? Magni nisi fuga voluptate, velit voluptas, eaque nobis cum deserunt eligendi reiciendis unde sit nesciunt.
-      </ModalContent>
-      <ModalActions>
-        <Button
-          border
-          className={styles.buttonCancel}
-          accent="negative"
-          onClick={() => alert("you pressed Cancel")}
-        >
-          Cancel
-        </Button>
-        <Button
-          variant="fill"
-          accent="positive"
-          onClick={() => alert("you pressed OK")}
-        >
-          OK
-        </Button>
-      </ModalActions>
-    </Modal>
+    <ColorModeComparison>
+      <Modal
+        trigger={<Button>Show</Button>}
+        title="Lorem Ipsum"
+        hideCloseIcon
+      >
+        <ModalContent>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, quaerat? A quos ab pariatur fugiat blanditiis, esse eum odit eligendi exercitationem voluptatem quod maiores nesciunt sapiente modi dolorem nisi accusamus architecto eius ipsa facere soluta? Magni nisi fuga voluptate, velit voluptas, eaque nobis cum deserunt eligendi reiciendis unde sit nesciunt.
+        </ModalContent>
+        <ModalActions>
+          <Button
+            border
+            className={styles.buttonCancel}
+            accent="negative"
+            onClick={() => alert("you pressed Cancel")}
+          >
+            Cancel
+          </Button>
+          <Button
+            variant="fill"
+            accent="positive"
+            onClick={() => alert("you pressed OK")}
+          >
+            OK
+          </Button>
+        </ModalActions>
+      </Modal>
+    </ColorModeComparison>
   ));
