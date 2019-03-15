5eaabea5ff3fc4e69681e5ed38b40ab6e28dbf51
devanshu <devanshu.g@santiment.net>
2019-03-15 13:26:18 +0530

    Combine Portal and modal components

--- src/Modal/Modal.js
+++ src/Modal/Modal.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
+import ReactDOM from "react-dom";
 import PropTypes from "prop-types";
 
 import ModalHeader from "./ModalHeader";
 import ModalActions from "./ModalActions";
-import Portal from "./Portal";
+import Panel from '../Panel/Panel';
 import styles from './Modal.module.scss';
 
 let mountNode = document.querySelector('#ui-modal');
@@ -23,14 +24,26 @@ class Modal extends Component {
 
   openModal = () => {
     const { onOpen } = this.props;
+
     mountNode.setAttribute('style', 'visibility:visible;');
-    this.setState({ open: true }, onOpen);
+    this.setState({ open: true }, () => {
+      onOpen();
+      mountNode
+        .querySelector(`.${styles.container}`)
+        .focus();
+    });
   };
 
   closeModal = () => {
     const { onClose } = this.props;
     mountNode.setAttribute('style', 'visibility:hidden;');
     this.setState({ open: false }, onClose);
+  };
+
+  handleOnKeyUp = e => {
+    if (e.keyCode == 27) {
+      this.closeModal();
+    }
   };
 
   render() {
@@ -51,21 +64,30 @@ class Modal extends Component {
         {React.cloneElement(trigger, {
           onClick: this.openModal
         })}
-        {open && (
-          <Portal closeModal={this.closeModal}>
-            <ModalHeader
-              onCloseModal={this.closeModal}
-              hideCloseIcon={hideCloseIcon}
-              title={title}
+        {open && ReactDOM.createPortal(
+          <React.Fragment>
+            <Panel variant="modal" className={styles.dialog}>
+              <ModalHeader
+                onCloseModal={this.closeModal}
+                hideCloseIcon={hideCloseIcon}
+                title={title}
+              />
+              {children}
+              <ModalActions
+                closeModal={this.closeModal}
+                onConfirmClick={onConfirmClick}
+                confirmLabel={confirmLabel}
+                cancelLabel={cancelLabel}
+              />
+            </Panel>
+            <div
+              tabIndex={0}
+              className={styles.container}
+              onClick={this.closeModal}
+              onKeyUp={this.handleOnKeyUp}
             />
-            {children}
-            <ModalActions
-              closeModal={this.closeModal}
-              onConfirmClick={onConfirmClick}
-              confirmLabel={confirmLabel}
-              cancelLabel={cancelLabel}
-            />
-          </Portal>
+          </React.Fragment>,
+          mountNode,
         )}
       </div>
     );
