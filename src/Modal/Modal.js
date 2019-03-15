717e66d3f9dc5d17aa86078e07e64351663db529
devanshu <devanshu.g@santiment.net>
2019-03-15 15:48:46 +0530

    Fix typo

--- src/Modal/Modal.js
+++ src/Modal/Modal.js
@@ -24,7 +24,7 @@ class Modal extends Component {
     window.addEventListener('keyup', this.onKeyUp)
   }
 
-  componentWillUnmout() {
+  componentWillUnmount() {
     window.removeEventListener('keyup', this.onKeyUp)
   }
 
