105182304c1e812a6b9dc5ea2fa4ac6f382f440f
DmitriVanGuard <dimavanguard@gmail.com>
2019-03-18 11:31:56 +0300

    Junk removed

--- src/Label/Label.js
+++ src/Label/Label.js
@@ -3,43 +3,16 @@ import PropTypes from 'prop-types'
 import cx from 'classnames'
 import styles from './Label.module.scss'
 
-/*
-   accents=[
-   black - default
-   light-grey
-   grey
-   dark-grey
-   red
-   blue
-   orange
-   green
-   cyan
-   ]
-
-   border
-
-   variant=[fill, round]
-*/
-const Label = ({
-  children,
-  variant,
-  empty,
-  border,
-  round,
-  fill,
-  accent,
-  as: Base
-}) => {
+const Label = ({ variant, accent, as: Base, ...props }) => {
   return (
     <Base
+      {...props}
       className={cx({
         [styles.label]: true,
         [styles[accent]]: accent,
         [styles[variant]]: variant
       })}
-    >
-      {children}
-    </Base>
+    />
   )
 }
 
