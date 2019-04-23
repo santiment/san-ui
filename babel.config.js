959ec89a683683f8bdda03245668d8e2e813b08f
DmitriVanGuard <dimavanguard@gmail.com>
2019-04-23 15:13:51 +0300

    fixed lib export

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,16 @@
-module.exports = function(api) {
+module.exports = function (api) {
   const presets = ['@babel/preset-env', '@babel/preset-react']
   const plugins = [
     '@babel/plugin-proposal-class-properties',
     '@babel/plugin-proposal-object-rest-spread',
-    '@babel/plugin-transform-object-assign'
+    '@babel/plugin-transform-object-assign',
+    '@babel/plugin-transform-react-constant-elements',
+    [
+      'babel-plugin-transform-react-remove-prop-types',
+      {
+        mode: 'unsafe-wrap'
+      }
+    ]
   ]
 
   if (api.env('test')) {
--- scripts/copy-files.js
+++ scripts/copy-files.js
@@ -1,21 +1,41 @@
-const fs = require('fs')
+const glob = require('glob')
 const path = require('path')
-const exec = require('child_process').execSync
+const fs = require('fs')
 
-const filesToCopyFromSrcRoot = [
-  'main.scss',
-  'mixins.scss',
-  'variables.scss',
-  'package.json'
-]
+const source = path.resolve(__dirname, '..', 'src')
+const destination = path.resolve(__dirname, '..', 'lib-build')
+
+if (!fs.existsSync(destination)) {
+  fs.mkdirSync(destination)
+}
+
+const filesToCopyFromSrcRoot = ['package.json']
 
 filesToCopyFromSrcRoot.forEach(fileName => {
   fs.copyFile(
-    path.resolve(__dirname, '..', 'src', fileName),
-    path.resolve(__dirname, '..', 'lib-build', fileName),
+    path.resolve(source, fileName),
+    path.resolve(destination, fileName),
     err => {
       if (err) throw err
       console.log(`👍 ${fileName} was copied into build`)
     }
   )
 })
+
+const copySCSSToBuildFolder = () => {
+  glob.sync(source + '/**/*.scss').forEach(fileName => {
+    const name = path.basename(fileName)
+    const folder = path.relative(source, path.dirname(fileName))
+    const resultFolderPath = path.resolve(destination, folder)
+    if (!fs.existsSync(resultFolderPath)) {
+      fs.mkdirSync(resultFolderPath)
+    }
+
+    fs.copyFile(fileName, path.resolve(resultFolderPath, name), err => {
+      if (err) throw err
+    })
+  })
+  console.log(`👍 scss files were copied into build`)
+}
+
+copySCSSToBuildFolder()
