badfb5ed7c40540dd275f48ed2003030b5c506d7
haritonasty <a.kharitonovaspb@gmail.com>
2019-07-03 12:55:36 +0300

    toggle defaultSelected if it was changing

--- src/Selector/Selector.js
+++ src/Selector/Selector.js
@@ -43,13 +43,20 @@ export class Selector extends Component {
     variant: PropTypes.oneOf(['border'])
   }
 
+  componentDidUpdate (prevProps) {
+    console.log(prevProps, this.props)
+    if (this.props.defaultSelected !== prevProps.defaultSelected) {
+      this.setState({ topic: this.props.defaultSelected })
+    }
+  }
+
   onSelectOption = newOption => {
     this.setState({ selected: newOption }, () => {
       this.props.onSelectOption(newOption)
     })
   }
 
-  render() {
+  render () {
     const { selected } = this.state
     const {
       variant,
