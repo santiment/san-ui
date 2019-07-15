1830bbb5b2308237042f69aa421c2ad01078c7d0
haritonasty <a.kharitonovaspb@gmail.com>
2019-07-15 13:12:24 +0300

    flash icon

---
+++ src/Icon/IconFlash.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default (
+  <svg xmlns='http://www.w3.org/2000/svg' width='12' height='15'>
+    <path d='M7.84 0h-3.8c-.4 0-.8.3-1 .7l-2 7c-.2.7.3 1.3.9 1.3h3.7l-.99 5.4c-.11.52.56.82.89.4l5.9-8.2c.5-.6 0-1.6-.8-1.6h-3l1.1-3.7c.2-.7-.3-1.3-.9-1.3z' />
+  </svg>
+)
