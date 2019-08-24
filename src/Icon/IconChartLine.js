607737d6ee25d03232a66f4cdb23c5cd7a08254e
DmitriVanGuard <dimavanguard@gmail.com>
2019-08-24 14:07:03 +0300

    Icon chart, line, bars

---
+++ src/Icon/IconChartLine.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default (
+  <svg xmlns='http://www.w3.org/2000/svg' width='14' height='8' stroke='#000'>
+    <path
+      d='M10 4c0-.552.168-1 .375-1h3.25c.207 0 .375.448.375 1s-.168 1-.375 1h-3.25C10.168 5 10 4.552 10 4zM0 4c0-.552.168-1 .375-1h3.25C3.832 3 4 3.448 4 4s-.168 1-.375 1H.375C.168 5 0 4.552 0 4z'
+      stroke='none'
+    />
+    <circle cx='7' cy='4' r='3.25' fill='none' strokeWidth='1.5' />
+  </svg>
+)
