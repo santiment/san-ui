7045683a1125ca04be45e517897a8800784f3e1f
haritonasty <a.kharitonovaspb@gmail.com>
2019-09-25 15:06:24 +0300

    add article icon

---
+++ src/Icon/IconArticle.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default (
+  <svg width='8' height='8' fill='none' xmlns='http://www.w3.org/2000/svg'>
+    <path d='M5.3.3H.8a.7.7 0 1 0 0 1.4h4.5a.8.8 0 0 0 0-1.4z' />
+    <path d='M7.3 3.3H.8a.8.8 0 0 0 0 1.5h6.5a.8.8 0 0 0 0-1.5z' />
+    <path d='M3.3 6.3H.8a.8.8 0 0 0 0 1.5h2.5a.8.8 0 0 0 0-1.5z' />
+  </svg>
+)
