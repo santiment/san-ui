16525c5c2bcfac4bddb1164980854c67c454e6e1
haritonasty <a.kharitonovaspb@gmail.com>
2019-09-25 15:15:13 +0300

    add ring icon

---
+++ src/Icon/IconRing.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default (
+  <svg width='8' height='8' fill='none' xmlns='http://www.w3.org/2000/svg'>
+    <path
+      fill-rule='evenodd'
+      clip-rule='evenodd'
+      d='M7.5 4a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0zM4 6a2 2 0 1 0 0-4 2 2 0 0 0 0 4z'
+    />
+  </svg>
+)
