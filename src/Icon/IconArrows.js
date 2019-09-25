a1599ec56c3472f387e3f372d90736115716c6f3
haritonasty <a.kharitonovaspb@gmail.com>
2019-09-25 15:08:53 +0300

    add arrows icon

---
+++ src/Icon/IconArrows.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default (
+  <svg width='10' height='6' fill='none' xmlns='http://www.w3.org/2000/svg'>
+    <path d='M2.2 5.4L.3 3.5a.7.7 0 0 1 0-1l2-1.9c.2-.2.6 0 .6.3v4.2c0 .4-.4.5-.7.3zM5 3.7a.7.7 0 1 1 0-1.4.7.7 0 0 1 0 1.4zM7 5V.9c0-.4.5-.5.8-.3l1.8 1.9c.3.3.3.7 0 1L7.8 5.4c-.3.2-.7 0-.7-.3z' />
+  </svg>
+)
