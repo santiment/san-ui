60c1419a8ecb1a6f60df020b9ee092be77688b10
haritonasty <a.kharitonovaspb@gmail.com>
2019-10-25 17:10:27 +0300

    add icons

---
+++ src/Icon/IconMail.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default (
+  <svg width='16' height='12' xmlns='http://www.w3.org/2000/svg'>
+    <path
+      fillRule='evenodd'
+      clipRule='evenodd'
+      d='M1 0h14a1 1 0 011 1v10a1 1 0 01-1 1H1a1 1 0 01-1-1V1a1 1 0 011-1zm.5 2.42v8.08h13V2.42l-5.36 4.6c-.66.55-1.62.55-2.28 0L1.5 2.41zm11.76-.92H2.74l5.1 4.37c.1.08.23.08.32 0l5.1-4.37z'
+    />
+  </svg>
+)
