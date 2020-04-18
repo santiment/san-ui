8509268a8bc1d97dabc3619a6e38c49bbf23fd2f
haritonasty <a.kharitonovaspb@gmail.com>
2020-04-18 16:53:50 +0300

    change tooltip position if cover trigger

--- src/Tooltip/TooltipContent.js
+++ src/Tooltip/TooltipContent.js
@@ -17,6 +17,21 @@ const getSignByPosition = position => {
   }
 }
 
+const getPositionsPrioritiesByPosition = position => {
+  switch (position) {
+    case 'top':
+      return ['top', 'bottom', 'right', 'left']
+    case 'bottom':
+      return ['bottom', 'top', 'right', 'left']
+    case 'left':
+      return ['left', 'right', 'top', 'bottom']
+    case 'right':
+      return ['right', 'left', 'top', 'bottom']
+    default:
+      return ['top', 'bottom', 'right', 'left']
+  }
+}
+
 const calculateAlignment = (triggerProp, tooltipProp, align) => {
   switch (align) {
     case 'start':
@@ -28,6 +43,63 @@ const calculateAlignment = (triggerProp, tooltipProp, align) => {
   }
 }
 
+const isCoverTriggerBorder = (triggerBorderPos, tooltipStart, tooltipEnd) => {
+  if (triggerBorderPos >= tooltipStart && triggerBorderPos <= tooltipEnd) {
+    return true
+  }
+
+  return false
+}
+
+const isCoverTrigger = ({
+  top,
+  bottom,
+  left,
+  right,
+  position,
+  triggerTop,
+  triggerBottom,
+  triggerLeft,
+  triggerRight
+}) => {
+  switch (position) {
+    case 'top':
+      return isCoverTriggerBorder(triggerTop, top, bottom)
+    case 'bottom':
+      return isCoverTriggerBorder(triggerBottom, top, bottom)
+    case 'left':
+      return isCoverTriggerBorder(triggerLeft, left, right)
+    case 'right':
+      return isCoverTriggerBorder(triggerRight, left, right)
+  }
+}
+
+const shiftFromBorders = (
+  rawTop,
+  rawLeft,
+  tooltipWidth,
+  tooltipHeight,
+  windowWidth,
+  windowHeight,
+  viewportOffset
+) => {
+  let top = rawTop
+  let left = rawLeft
+
+  if (left < viewportOffset) {
+    left = viewportOffset
+  } else if (left + tooltipWidth > windowWidth - viewportOffset) {
+    left = windowWidth - viewportOffset - tooltipWidth
+  }
+  if (top < viewportOffset) {
+    top = viewportOffset
+  } else if (top + tooltipHeight > windowHeight - viewportOffset) {
+    top = windowHeight - viewportOffset - tooltipHeight
+  }
+
+  return { top, left }
+}
+
 class TooltipContent extends PureComponent {
   tooltipRef = React.createRef()
 
@@ -35,14 +107,19 @@ class TooltipContent extends PureComponent {
     this.forceUpdate()
   }
 
-  getTooltipCoordinates (trigger, tooltipHeight, tooltipWidth) {
-    const { position, offsetX, offsetY, align } = this.props
-    const {
-      width: triggerWidth,
-      height: triggerHeight,
-      left: triggerLeft,
-      top: triggerTop
-    } = trigger.getBoundingClientRect()
+  getTooltipCoordinates ({
+    triggerWidth,
+    triggerHeight,
+    triggerLeft,
+    triggerTop,
+    tooltipHeight,
+    tooltipWidth,
+    windowWidth,
+    windowHeight,
+    viewportOffset,
+    position
+  }) {
+    const { offsetX, offsetY, align } = this.props
 
     const sign = getSignByPosition(position)
     let top = triggerTop
@@ -58,7 +135,22 @@ class TooltipContent extends PureComponent {
         (position === 'left' ? -tooltipWidth : triggerWidth) + sign * offsetX
     }
 
-    return { top, left }
+    const { top: shiftedTop, left: shiftedLeft } = shiftFromBorders(
+      top,
+      left,
+      tooltipWidth,
+      tooltipHeight,
+      windowWidth,
+      windowHeight,
+      viewportOffset
+    )
+
+    return {
+      top: shiftedTop,
+      left: shiftedLeft,
+      bottom: shiftedTop + tooltipHeight,
+      right: shiftedLeft + tooltipWidth
+    }
   }
 
   getTooltipStyles () {
@@ -80,26 +172,55 @@ class TooltipContent extends PureComponent {
 
     const { offsetWidth: tooltipWidth, offsetHeight: tooltipHeight } = tooltip
 
-    let { top, left } = this.getTooltipCoordinates(
-      trigger,
-      tooltipHeight,
-      tooltipWidth
+    const {
+      width: triggerWidth,
+      height: triggerHeight,
+      left: triggerLeft,
+      top: triggerTop
+    } = trigger.getBoundingClientRect()
+
+    const positionPriorities = getPositionsPrioritiesByPosition(
+      this.props.position
     )
 
-    if (left < viewportOffset) {
-      left = viewportOffset
-    } else if (left + tooltipWidth > windowWidth - viewportOffset) {
-      left = windowWidth - viewportOffset - tooltipWidth
-    }
-    if (top < viewportOffset) {
-      top = viewportOffset
-    } else if (top + tooltipHeight > windowHeight - viewportOffset) {
-      top = windowHeight - viewportOffset - tooltipHeight
+    let topRes, leftRes
+
+    for (let i = 0; i < 4; i++) {
+      const { top, left, bottom, right } = this.getTooltipCoordinates({
+        triggerWidth,
+        triggerHeight,
+        triggerLeft,
+        triggerTop,
+        tooltipHeight,
+        tooltipWidth,
+        windowWidth,
+        windowHeight,
+        viewportOffset,
+        position: positionPriorities[i]
+      })
+
+      if (
+        !isCoverTrigger({
+          top,
+          left,
+          bottom,
+          right,
+          position: positionPriorities[i],
+          triggerTop,
+          triggerLeft,
+          triggerBottom: triggerTop + triggerHeight,
+          triggerRight: triggerLeft + triggerWidth
+        })
+      ) {
+        topRes = top
+        leftRes = left
+        break
+      }
     }
 
     return {
-      top: `${top + scrollY}px`,
-      left: `${left + scrollX}px`
+      top: `${topRes + scrollY}px`,
+      left: `${leftRes + scrollX}px`
     }
   }
 
