c11912dd71f98aa8b429b436d29ad41d77df9afd
haritonasty <a.kharitonovaspb@gmail.com>
2020-09-10 19:37:06 +0300

    upd pointer and report ions

--- src/Icon/IconPointerRight.js
+++ src/Icon/IconPointerRight.js
@@ -3,13 +3,13 @@ import React from 'react'
 export default (
   <svg
     xmlns='http://www.w3.org/2000/svg'
-    width='17'
+    width='16'
     height='9'
-    viewBox='0 0 17 9'
+    viewBox='0 0 16 9'
   >
     <path
       fillRule='evenodd'
-      d='M15.61 4.69a.8.8 0 000-1.37L10.28.12A.8.8 0 009.07.8v2.7H.5a.5.5 0 100 1h8.57v2.7c0 .61.67 1 1.2.68l5.34-3.2z'
+      d='M.5 3.5a.5.5 0 000 1h13.4l-3.7 2.6a.5.5 0 10.6.8l5-3.5a.5.5 0 000-.8l-5-3.5a.5.5 0 00-.6.8L14 3.5H.5z'
       clipRule='evenodd'
     />
   </svg>
--- src/Icon/IconPointerRightSmall.js
+++ src/Icon/IconPointerRightSmall.js
@@ -3,13 +3,13 @@ import React from 'react'
 export default (
   <svg
     xmlns='http://www.w3.org/2000/svg'
-    width='8'
-    height='6'
-    viewBox='0 0 8 6'
+    width='10'
+    height='5'
+    viewBox='0 0 10 5'
   >
     <path
       fillRule='evenodd'
-      d='M7.8 3.4a.5.5 0 000-.8L4.63.1a.5.5 0 00-.81.4v2H.5a.5.5 0 000 1h3.3v2c0 .41.49.65.82.4L7.8 3.4z'
+      d='M.5 2a.5.5 0 100 1h7.6L6.8 4.1a.5.5 0 00.7.8l2.3-2a.5.5 0 000-.8L7.5.1a.5.5 0 00-.7.8L8.1 2H.5z'
       clipRule='evenodd'
     />
   </svg>
