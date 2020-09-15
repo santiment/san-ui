96d81ea7332c03bc12834ada9c5c856dd461bfd1
Nastya Kharitonova <a.kharitonovaspb@gmail.com>
2020-09-15 14:39:44 +0300

    return chart line to stroke

--- src/Icon/IconChartLine.js
+++ src/Icon/IconChartLine.js
@@ -5,12 +5,16 @@ export default (
     width='14'
     height='10'
     viewBox='0 0 14 10'
+    fill='none'
     xmlns='http://www.w3.org/2000/svg'
+    stroke='#000'
   >
     <path
-      fillRule='evenodd'
-      clipRule='evenodd'
-      d='M10.8 5a3.8 3.8 0 11-7.6 0 3.8 3.8 0 017.6 0zm1.175.5a5 5 0 01-9.95 0H.5a.5.5 0 010-1h1.525a5 5 0 019.95 0H13.5a.5.5 0 010 1h-1.525z'
+      d='M11 5C11 7.20914 9.20914 9 7 9C4.79086 9 3 7.20914 3 5M11 5C11 2.79086 9.20914 1 7 1C4.79086 1 3 2.79086 3 5M11 5H13M3 5H1'
+      fill='none'
+      stroke-width='1.2'
+      stroke-linecap='round'
+      stroke-linejoin='round'
     />
   </svg>
 )
